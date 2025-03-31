cb88836aa0d5ad2f0a01a88c2de66de8f010e916
Bruno Bueno <ironboy318@gmail.com>
2025-03-31 13:14:22 -0300

    Corrigir nome do repositório para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/cantinhofefo/' : '/',
+  base: mode === 'production' ? '/cantinhodofefo/' : '/',
   server: {
     host: "::",
     port: 8080,
